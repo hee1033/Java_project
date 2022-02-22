42492dacb2ffa77b62880da9be6783a75da35731
hee1033 <99076538+hee1033@users.noreply.github.com>
2022-02-22 02:02:52 +0000

    java_study

---
+++ day03/If활용02.java
@@ -0,0 +1,15 @@
+public class Main{
+  public static void main(String[] args){
+    int age = 20;
+
+    if(age <= 19){
+      System.out.println("할인 대상 입니다.");
+    }
+    else if(age >= 60){
+      System.out.println("할인 대상 입니다.");
+    }
+    else{
+    System.out.println("할인 대상이 아닙니다.");
+    }
+  }
+}
